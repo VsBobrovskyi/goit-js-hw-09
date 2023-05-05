bc657aa3d86697a88716a5318e0171676d1e7d83
VsBobrovskyi <119082116+VsBobrovskyi@users.noreply.github.com>
2023-05-05 11:49:06 +0000

    Deploying to gh-pages from @ VsBobrovskyi/goit-js-hw-09@f8d813a3eac13f1f99f72d47070a712fb113852e 🚀

---
+++ 01-color-switcher.35faaa02.js
@@ -0,0 +1,2 @@
+!function(){function t(){return"#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}var e=document.querySelector("button[data-start]"),n=document.querySelector("button[data-stop]"),o=null;e.addEventListener("click",(function(){document.body.style.backgroundColor=t(),e.disabled=!0,n.disabled=!1,o=setInterval((function(){document.body.style.backgroundColor=t()}),1e3)})),n.addEventListener("click",(function(){clearInterval(o),e.disabled=!1,n.disabled=!0})),n.disabled=!0}();
+//# sourceMappingURL=01-color-switcher.35faaa02.js.map
